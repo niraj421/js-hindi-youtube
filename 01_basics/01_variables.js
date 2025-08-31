8b2cfd76d39aba521bbfc7e408e6ff0d1405efbf
Niraj kumar singh <nirajup.6388@gmail.com>
2025-08-31 06:39:22 +0000

    variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,18 @@
+const accountId = 14453
+let accountEmail = "nirajsigh123"
+accountcity = "jaipur"
+let accountState;
+//accountId = 2 //not allowed
+
+accountEmail = "hcahc.com"
+accountpassword = "21212121"
+accountCity = "bengaluru"
+
+console.log(accountId);
+
+/*
+prefer not to use var
+becauuse of issue in block scope and functional scoope
+
+
+*/
