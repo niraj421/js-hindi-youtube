139c7b7b52075703cdba6c1151bec47e6df7c108
Niraj kumar singh <nirajup.6388@gmail.com>
2025-08-31 06:37:19 +0000

    conversion of data type

---
+++ 01_basics/03_conversionoperation.js
@@ -0,0 +1,27 @@
+let score = "hitesh"
+
+//console.log(typeof score);
+//console.log(typeof(score));
+
+let valueInNumber = Number(score)
+
+//console.log(typeof valueInNumber );
+//console.log(valueInNumber );
+
+ // "33" => 33
+// "33abc" => NaN
+// True => 1: false => ""
+
+let isLoggedIn = "niraj"
+let boolleanIsLoggedIn = Boolean(isLoggedIn)
+//console.log(boolleanIsLoggedIn);
+
+//1 => true;  0=> false
+// "" => fasle
+// "hitesh" => true
+
+
+let someNumber  = 33
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
