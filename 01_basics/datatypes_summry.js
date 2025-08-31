17c8f76f5646ac0c9ff7eeddf7cbb8e2e86bba19
Niraj kumar singh <nirajup.6388@gmail.com>
2025-08-31 13:36:40 +0000

    datatype  summry

--- 01_basics/datatypes_summry.js
+++ 01_basics/datatypes_summry.js
@@ -29,4 +29,33 @@ const myFunction = function(){
     console.log("hello world");
 }
 
-console.log(typeof myFunction);
+console.log(typeof anotherId);
+
+// link to study
+
+
+//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+// stack(primitive),Heap(Non-primitive)
+
+let myYoutubename = "nirajsinghdotcom"
+
+let anothername = myYoutubename
+
+anothername = "chaiourcode"
+
+console.log(myYoutubename);
+console.log(anothername);
+
+let userOne = {
+    email: "user@google.com",
+    upi: "user@ybl"
+
+}
+let userTwo = userOne 
+
+userTwo.email = "niraj@1234"
+
+console.log(userOne.email);
+console.log(userTwo.email);
+
+
