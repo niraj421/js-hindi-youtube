0db02e2b5adbe97b84ee1e59f944e22ac5d9e0e3
Niraj kumar singh <nirajup.6388@gmail.com>
2025-09-01 18:37:11 +0000

    json talk

---
+++ 02_basics/04_objects.js
@@ -0,0 +1,80 @@
+//const tinderUser = new Object() //singleton object
+//const tinderUser = {}  //non singleton object
+ const tinderUser =  {}
+
+tinderUser.id  = "123abc"
+tinderUser.name = "Sammy"
+tinderUser.isLoggedIn = false
+
+
+//console.log(tinderUser);
+
+const regularUser = {
+    email: "soome@gmail.com",
+    fullname: {
+        userFullname:{
+            firstname: "Niraj",
+            Lastname: "choudhary",
+
+        }
+    }
+}
+//console.log(regularUser.fullname.userFullname.firstname);
+
+const obj1 = {1:"a",2:"b",}
+const obj2 = {3:"a",4:"b",}
+const obj4 = {4:"a",9:"b",}
+//const obj3 = {obj1,obj2}
+//const obj3 = Object.assign({},obj1,obj2,obj4)
+
+const obj3 = {...obj1,...obj2,...obj4}
+//console.log(obj3);
+
+const users = [
+    {
+    },
+    {
+    },
+    {
+    },
+]
+
+users[1].email
+
+// console.log(tinderUser);
+
+// console.log(Object.keys(tinderUser));
+// console.log(Object.values(tinderUser));
+// console.log(Object.entries(tinderUser));
+
+// console.log(tinderUser.hasOwnProperty('isLoggedIn'));  
+
+
+const course = {
+    coursename: "js in hindi",
+    price: "999",
+    courseInstructor: "hitesh"
+
+}
+
+// course.courseInstructor
+
+const {courseInstructor: instructor} = course
+
+console.log(instructor);
+
+
+// {
+//     "name": "hiiitesh",
+//     "coursename": "js in hindi ",
+//     "price": "free"
+// }
+
+[
+    {},
+    {},
+    {}
+]
+
+
+
