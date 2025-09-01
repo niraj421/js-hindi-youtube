2dc60073e6b327650818e121eb4a9cabf4614944
Niraj kumar singh <nirajup.6388@gmail.com>
2025-09-01 18:36:11 +0000

    object

---
+++ 02_basics/03_objects.js
@@ -0,0 +1,35 @@
+// singleton 
+//Object.create 
+
+//object literals 
+const mySym = Symbol("key1")
+const JsUser = {
+    name: "Hitesh",
+    "full name": "Niraj singh",
+    [mySym]: "myKey1",
+    location: "jaipur",
+    email: "hitesh@google.com",
+    isLoggedIn: false,
+    lastLoginginDays: ["Monday","Sunday"]
+    
+}
+//console.log(JsUser.email);
+//console.log(JsUser["email"]);
+//console.log(JsUser["full name"])
+//console.log(JsUser[mySym]);
+
+JsUser.email = "hitesh@chatgpt.com"
+//Object.freeze(JsUser)
+JsUser.email = "hitesh@microsoft.com"
+//console.log(JsUser);
+
+JsUser.greeting = function(){
+    console.log("hello js user");
+    
+}
+JsUser.greetingTwo = function(){
+    console.log(`hello js user, ${this.name}` );
+    
+}
+console.log(JsUser.greeting());
+console.log(JsUser.greetingTwo());
