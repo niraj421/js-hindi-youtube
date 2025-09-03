3cc2a7cbed2991c0f0d245e2700662d52f1d67e2
Niraj kumar singh <nirajup.6388@gmail.com>
2025-09-03 20:32:03 +0000

    loop ya iteration

---
+++ 05_loops ya iterations/four.js
@@ -0,0 +1,30 @@
+const myObject = {
+    js: 'javascript',
+    cpp: 'c++',
+    rb: "ruby",
+    swift: "swift by apple"
+
+}
+
+//   for in
+
+for (const key in myObject) {
+//console.log(`${key} shortcut is for ${myObject[key]}`);
+
+}
+
+const programming = ["js", "rb", "py", "java","cpp"]
+for (const key in programming) {
+//console.log(programming[key]);
+}
+
+// const map = new Map()
+// map.set('IN',"india")
+// map.set('USA',"united states of America")
+// map.set('Fr',"France")
+// map.set('IN',"India")
+
+// for (const key in map) {//    ===== map ke andar kabhi bhi for in loop work nahi karta hai
+//   console.log(map);
+  
+// }
