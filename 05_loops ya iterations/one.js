359315e0c23091187db4290bdce3cc34e73274a3
Niraj kumar singh <nirajup.6388@gmail.com>
2025-09-03 18:11:55 +0000

    llop ya itteration

---
+++ 05_loops ya iterations/one.js
@@ -0,0 +1,54 @@
+// for 
+for (let i = 0; i <= 10 ; i++) {
+    const element = i;
+    if (element == 5){
+       // console.log("5 is best number");
+    }
+  //  console.log(element);
+}
+
+//console.log(element);
+
+for (let i = 1; i <= 10;i++) {
+   // console.log(`outer loop value: ${i}`);
+for (let j = 1; j <= 10; j++) {
+//console.log(`innner loop value ${j} and inner {i}`);
+
+//console.log(i + '*' + j + '=' + i*j);
+}
+}
+let myArray = ["flash","batman","superman"]
+//onsole.log(myArray.length);
+
+
+for (let index = 0; index <= myArray.length; index++) {
+    const element = myArray[index];
+//    console.log(element);
+    
+}
+
+
+// // break and continue
+// for (let i = 0; i <= 20; i++) {
+//     if( i==5){
+//         console.log(`Detected 5`);
+//         break;
+
+        
+//     }
+//     console.log(`value of i is ${i}`);
+    
+// }
+
+
+// break and continue
+for (let i = 0; i <= 20; i++) {
+    if( i==5){
+        console.log(`Detected 5`);
+        continue
+
+        
+    }
+    console.log(`value of i is ${i}`);
+    
+}
